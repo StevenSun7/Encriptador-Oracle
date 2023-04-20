64d10f12f12d1489a8eb463223f2211c8c33d4f2
StevenSun7 <estebanpaez7@gmail.com>
2023-04-20 16:36:13 -0300

    Comenzamos con lo Responsivo

---
+++ Encriptador/js/index.js
@@ -0,0 +1,11 @@
+// ingresar texto
+const textareaInput = document.getElementById('Ingresar-Texto');
+
+// botones Encriptar y Desencriptar
+const encryptButton = document.getElementById('botones-E');
+const decryptButton = document.getElementById('botones-D');
+
+// devolución del texto ingresado
+const textareaOutput = document.getElementById('Área-Traducción');
+const copyButton = document.getElementById('boton-copiar');
+
