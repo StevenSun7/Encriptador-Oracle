c936ad2ca754aa3754d8019b848aff5919a609c3
StevenSun7 <estebanpaez7@gmail.com>
2023-04-21 18:46:09 -0300

    Terminamos js

--- Encriptador/js/index.js
+++ Encriptador/js/index.js
@@ -1,11 +1,57 @@
-// ingresar texto
-const textareaInput = document.getElementById('Ingresar-Texto');
+var textoOrigen = document.getElementById("mensaje-ingresado");
+var textoEncriptado = document.getElementById("devolucion-mensaje");
+var accionEncriptar = document.querySelector("#encriptar");
+var accionDesencriptar = document.querySelector("#desencriptar");
+var accionCopiar = document.querySelector("#copiar")
+textoEncriptado.disabled = true
 
-// botones Encriptar y Desencriptar
-const encryptButton = document.getElementById('botones-E');
-const decryptButton = document.getElementById('botones-D');
+accionEncriptar.onclick = function() {
+    var textoEncriptado = textoOrigen.value.toLowerCase();
+    let resultadotexto = textoEncriptado.replace (/[aeiou]/, function (letra) { 
+        switch(letra) {
+            case 'a':
+                return "ai"
+            case 'e':
+                return "enter"
+            case 'i':
+                return "imes"
+            case 'o':
+                return "ober";
+            case 'u':
+                return "ufat"
+            default:
+                break;
+        }
+    })        
+    document.getElementById("devolucion-mensaje").value = resultadotexto;
+    document.getElementById("mensaje-ingresado").value = "";
+};
 
-// devolución del texto ingresado
-const textareaOutput = document.getElementById('Área-Traducción');
-const copyButton = document.getElementById('boton-copiar');
+accionDesencriptar.onclick = function() {
+    var textoEncriptado = textoOrigen.value.toLowerCase();
+    let resultadotexto = textoEncriptado.replace(/(ai|enter|imes|ober|ufat)/, function (letra) { 
+        switch(letra) {
+            case 'ai':
+                return "a"
+            case 'enter':
+                return "e"
+            case 'imes':
+                return "i"
+            case 'ober':
+                return "o";
+            case 'ufat':
+                return "u"
+            default:
+                break;
+        }
+    }) 
+    document.getElementById("devolucion-mensaje").value = resultadotexto;
+    document.getElementById("mensaje-ingresado").value = "";
+};
 
+accionCopiar.onclick = function () {
+    var copiarEncriptado = document.getElementById("devolucion-mensaje").value
+    navigator.clipboard.writeText(copiarEncriptado);
+    document.getElementById("mensaje-ingresado").value = copiarEncriptado
+    document.getElementById("devolucion-mensaje").value = "";
+}
