0c3a1cc82b6ee3e1c8c48cd0f53706479066888a
StevenSun7 <estebanpaez7@gmail.com>
2023-04-26 18:18:31 -0300

    Dando retoques

--- js/index.js
+++ js/index.js
@@ -1,67 +1,85 @@
 var textoOrigen = document.getElementById("mensaje-ingresado");
 var textoEncriptado = document.getElementById("devolucion-mensaje");
+var background = document.querySelector(".Área-Traducción");
 var accionEncriptar = document.querySelector("#encriptar");
 var accionDesencriptar = document.querySelector("#desencriptar");
-var accionCopiar = document.querySelector("#copiar")
-textoEncriptado.disabled = true
-
-
+var accionCopiar = document.querySelector("#copiar");
+textoEncriptado.disabled = true;
 
 accionEncriptar.onclick = function() {
+    ocultarBackground();
     var textoEncriptado = textoOrigen.value.toLowerCase();
-    let resultadotexto = textoEncriptado.replace (/[a,e,i,o,u]/g, function (letra) { 
-        switch(letra) {
-            case 'a':
-                return "ai"
-            case 'e':
-                return "enter"
-            case 'i':
-                return "imes"
-            case 'o':
+    if (textoEncriptado === "") {
+        background.classList.remove("cambiarBackground");
+        textoEncriptado.classList.remove("cambiarBackground");
+        return;
+    }
+    let resultadotexto = textoEncriptado.replace(/[aeiou]/g, function(letra) {
+        switch (letra) {
+            case "a":
+                return "ai";
+            case "e":
+                return "enter";
+            case "i":
+                return "imes";
+            case "o":
                 return "ober";
-            case 'u':
-                return "ufat"
+            case "u":
+                return "ufat";
             default:
                 break;
         }
-    }) 
+    });
     document.getElementById("devolucion-mensaje").value = resultadotexto;
-    document.getElementById("mensaje-ingresado").value = "";
+    textoOrigen.value = "";
 };
 
-function eliminarAcentos(texto) {
-    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+function ocultarBackground() {
+    background.classList.add("cambiarBackground");
+    textoEncriptado.classList.add("cambiarBackground");
 }
-var textarea = document.querySelector('textarea');
-textarea.addEventListener('input', function(event) {
+
+
+function eliminarAcentos(texto) {
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+var textarea = document.querySelector("textarea");
+textarea.addEventListener("input", function(event) {
     var textoSinAcentos = eliminarAcentos(event.target.value);
     event.target.value = textoSinAcentos;
 });
 
 
 accionDesencriptar.onclick = function() {
+    ocultarBackground();
     var textoEncriptado = textoOrigen.value.toLowerCase();
-    let resultadotexto = textoEncriptado.replace(/(ai|enter|imes|ober|ufat)/g, function (letra) { 
-        switch(letra) {
-            case 'ai':
-                return "a"
-            case 'enter':
-                return "e"
-            case 'imes':
-                return "i"
-            case 'ober':
+    if (textoEncriptado === "") {
+        background.classList.remove("cambiarBackground");
+        textoEncriptado.classList.remove("cambiarBackground");
+        return;
+    }
+    let resultadotexto = textoEncriptado.replace(/(ai|enter|imes|ober|ufat)/g, function(letra) {
+        switch (letra) {
+            case "ai":
+                return "a";
+            case "enter":
+                return "e";
+            case "imes":
+                return "i";
+            case "ober":
                 return "o";
-            case 'ufat':
-                return "u"
+            case "ufat":
+                return "u";
             default:
                 break;
         }
-    }) 
+    });
     document.getElementById("devolucion-mensaje").value = resultadotexto;
-    document.getElementById("mensaje-ingresado").value = "";
+    textoOrigen.value = "";
 };
 
-accionCopiar.onclick = function () {
-    var copiarEncriptado = document.getElementById("devolucion-mensaje").value
+accionCopiar.onclick = function() {
+    var copiarEncriptado = document.getElementById("devolucion-mensaje").value;
     navigator.clipboard.writeText(copiarEncriptado);
-}
+};
